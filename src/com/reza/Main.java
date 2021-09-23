55a140dd2ba5e34b172685dfb32647aa5aa142cb
Naim Reza <naim.reza@cellosco.pe>
2021-09-23 12:01:14 +0600

    write test for ImageToText class

--- src/com/reza/Main.java
+++ src/com/reza/Main.java
@@ -1,8 +1,19 @@
 package com.reza;
 
+import java.io.File;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        ImagetoText imagetoText = new ImageToText();
+        File inputFile = new File("cat.jpg");
+        String outputFileName = "cat_out.jpg";
+
+        //take the image file and convert it to string
+        String convertedString = imagetoText.convertToString(inputFile);
+
+        //write the string to a file
+        imagetoText.writeFile(outputFileName);
+
     }
 }
