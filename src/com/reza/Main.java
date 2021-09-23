6e687d161f12e6e212212101b5ee07faee4e9749
Naim Reza <naim.reza@cellosco.pe>
2021-09-23 12:02:03 +0600

    fix classname issue

--- src/com/reza/Main.java
+++ src/com/reza/Main.java
@@ -5,15 +5,15 @@ import java.io.File;
 public class Main {
 
     public static void main(String[] args) {
-        ImagetoText imagetoText = new ImageToText();
+        ImageToText imageToText = new ImageToText();
         File inputFile = new File("cat.jpg");
         String outputFileName = "cat_out.jpg";
 
         //take the image file and convert it to string
-        String convertedString = imagetoText.convertToString(inputFile);
+        String convertedString = imageToText.convertToString(inputFile);
 
         //write the string to a file
-        imagetoText.writeFile(outputFileName);
+        imageToText.writeFile(outputFileName);
 
     }
 }
