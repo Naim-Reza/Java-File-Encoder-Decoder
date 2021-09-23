5e338efc988edceb139914dac87ffdff8928ae71
Naim Reza <naim.reza@cellosco.pe>
2021-09-23 12:41:12 +0600

    implement convertToString method

--- src/com/reza/ImageToText.java
+++ src/com/reza/ImageToText.java
@@ -1,5 +1,28 @@
 package com.reza;
 
+import javax.imageio.stream.ImageInputStream;
+import java.io.*;
+
 public class ImageToText {
     private String encodedString = null;
+
+    public String convertToString(File inputFile) {
+        try{
+            FileInputStream fileInputStream = new FileInputStream(inputFile);
+            int fileSize = (int) inputFile.length();
+            byte[] imageData = new byte[fileSize];
+            fileInputStream.read(imageData);
+            return imageData.toString();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public void writeFile(String outputFileName) {
+
+    }
 }
--- src/com/reza/Main.java
+++ src/com/reza/Main.java
@@ -7,13 +7,14 @@ public class Main {
     public static void main(String[] args) {
         ImageToText imageToText = new ImageToText();
         File inputFile = new File("cat.jpg");
-        String outputFileName = "cat_out.jpg";
+        String outputFileName = "cat_out.txt";
 
         //take the image file and convert it to string
         String convertedString = imageToText.convertToString(inputFile);
+        System.out.println(convertedString);
 
         //write the string to a file
-        imageToText.writeFile(outputFileName);
+        //imageToText.writeFile(outputFileName);
 
     }
 }
